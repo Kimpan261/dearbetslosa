62b72aea2f5fa8bb9688946cdce48ac83213e355
Kimpan261 <124882104+Kimpan261@users.noreply.github.com>
2023-05-11 13:14:19 +0200

    Pack

---
+++ DeArbetslosa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeArbetslosa.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

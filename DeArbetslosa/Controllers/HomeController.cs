871bd0acc7c72d6527031ffbc5e51eaea304200e
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-05-12 14:34:11 +0200

    basic flight list

--- DeArbetslosa/Controllers/HomeController.cs
+++ DeArbetslosa/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿using DeArbetslosa.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Xml;
 
 namespace DeArbetslosa.Controllers
 {
 	public class HomeController : Controller
 	{
+
 		private readonly ILogger<HomeController> _logger;
 
 		public HomeController(ILogger<HomeController> logger)
@@ -13,9 +20,12 @@ namespace DeArbetslosa.Controllers
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+		public async Task <IActionResult> Index()
 		{
-			return View();
+			string r = await MakeRequest();
+			Arrival a = new Arrival();
+			a = JsonConvert.DeserializeObject<Arrival>(r);
+            return View(a);
 		}
 
 		public IActionResult Privacy()
@@ -28,5 +38,34 @@ namespace DeArbetslosa.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		public async Task<string> MakeRequest()
+		{
+			string t = DateTime.Now.ToString("yyyy-MM-dd");
+			string IATA = "GOT";
+
+			var client = new HttpClient();
+			client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+			client.DefaultRequestHeaders.CacheControl = CacheControlHeaderValue.Parse("no-cache");
+			client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "b137901e98094345a598042dbdc5827d"); //TODO external key
+
+			var uri = "https://api.swedavia.se/flightinfo/v2/" + IATA + "/arrivals/" + t;
+
+			var response = await client.GetAsync(uri);
+			var responseContent = await response.Content.ReadAsStringAsync();
+			return responseContent;
+			
+			
+			/*
+			a = JsonConvert.DeserializeObject<Arrival>(responseContent);	
+            await Console.Out.WriteLineAsync(a.NumberOfFlights);
+			await Console.Out.WriteLineAsync(responseContent);
+
+            await Console.Out.WriteLineAsync(a.Flights[1].ArrivalTime.ScheduledUtc.ToString());
+            await Console.Out.WriteLineAsync(a.Flights[1].DepartureAirportEnglish);
+            await Console.Out.WriteLineAsync(a.Flights[1].FlightId);
+            await Console.Out.WriteLineAsync(a.Flights[1].FlightLegStatusEnglish);
+			*/
+        }
 	}
 }
---
+++ DeArbetslosa/Models/Arrival.cs
@@ -0,0 +1,31 @@
+﻿namespace DeArbetslosa.Models
+{
+	public class Arrival
+	{
+		public string NumberOfFlights { get; set; }
+		public F[] Flights { get; set; }
+
+		public class F // Flights
+		{
+			public string DepartureAirportEnglish;
+			public string FlightId;
+			public string Status;
+			public AT ArrivalTime;
+			public LAS LocationAndStatus;
+
+			public class LAS
+			{
+			public string FlightLegStatusEnglish;
+			}
+
+			public class AT //ArrivalTime
+			{
+				public DateTime ScheduledUtc;
+			}
+		}
+		public Arrival()
+		{
+
+		}
+	}
+}

17eb9795ae5797966a0fa1f69c5ab48d60fbb61d
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-05-17 11:00:56 +0200

    Arrivals select date

--- DeArbetslosa/Controllers/ArrivalsController.cs
+++ DeArbetslosa/Controllers/ArrivalsController.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http.Headers;
+using System.Xml.Linq;
 
 namespace DeArbetslosa.Controllers
 {
 	public class ArrivalsController : Controller
 	{
-
 		private readonly ILogger<ArrivalsController> _logger;
 
 		public ArrivalsController(ILogger<ArrivalsController> logger)
@@ -18,22 +18,26 @@ namespace DeArbetslosa.Controllers
 
 		public async Task <IActionResult> Index()
 		{
-			string r = await MakeRequest();
-			Arrivals a = new Arrivals();
-			a = JsonConvert.DeserializeObject<Arrivals>(r);
-            return View(a);
+
+			return View(JsonConvert.DeserializeObject<Arrivals>(await MakeRequest(DateTime.Now)));
 		}
 
-		
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public async Task<IActionResult> SetDate(string date)
+		{
+			//defaults to today
+			if (string.IsNullOrEmpty(date)) return View("Index", JsonConvert.DeserializeObject<Arrivals>(await MakeRequest(DateTime.Now))); 
+            return View("Index", JsonConvert.DeserializeObject<Arrivals>(await MakeRequest(DateTime.Parse(date)))); 
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 
-		public async Task<string> MakeRequest()
+		public async Task<string> MakeRequest(DateTime d)
 		{
-			string t = DateTime.Now.ToString("yyyy-MM-dd");
+			string t = d.ToString("yyyy-MM-dd");
 			string IATA = "GOT";
 
 			var client = new HttpClient();

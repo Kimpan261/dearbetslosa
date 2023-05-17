c5ad31b66769e712a25cbc83f72aae339ddcc270
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-05-17 11:28:42 +0200

    Arrivals sorted by date, deleted flights hidden

--- DeArbetslosa/wwwroot/js/site.js
+++ DeArbetslosa/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//ARRIVALS / DEPARTURE
+function sortTable() {
+    console.log("ye");
+    const table = document.getElementById("arrivalsTable"); // TODO for departure later
+    const rows = table.getElementsByTagName("tr");
+    const dateColumnIndex = 0;
+    const sortTableByDate = () => {
+        const sortedRows = Array.from(rows).slice(1); // Exclude the table header row
+        sortedRows.sort((rowA, rowB) => {
+            const dateA = new Date(rowA.cells[dateColumnIndex].textContent);
+            const dateB = new Date(rowB.cells[dateColumnIndex].textContent);
+            return dateA - dateB;
+        });
+
+        while (table.rows.length > 1) {
+            table.deleteRow(1); // Remove existing rows, starting from index 1
+        }
+
+        sortedRows.forEach((row) => {
+            table.appendChild(row); // Append sorted rows back to the table
+        });
+    };
+
+    // Add click event listener to the date column header
+    //const dateHeader = table.rows[0].cells[dateColumnIndex];
+    //dateHeader.addEventListener("click", sortTableByDate);
+    sortTableByDate();
+}
+
